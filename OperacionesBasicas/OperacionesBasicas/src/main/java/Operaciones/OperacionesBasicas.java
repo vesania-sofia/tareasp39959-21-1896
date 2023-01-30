1c5572572a8f157cd8f7731ee5c4caaa6c575955
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-01-30 15:35:30 -0600

    revision y documentacion de programa

--- OperacionesBasicas/OperacionesBasicas/src/main/java/Operaciones/OperacionesBasicas.java
+++ OperacionesBasicas/OperacionesBasicas/src/main/java/Operaciones/OperacionesBasicas.java
@@ -18,15 +18,21 @@ public class OperacionesBasicas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // declaramos las variables para la opciones, los datos numericos y el resultado
         float opc=0,dato1=0,dato2=0,result=0;
-
+        // imprime en pantalla las ociones de operar
         System.out.println("1.-sumar    2.-restar   3.-multiplicar  4.-dividir  otro numero.-salir");
         System.out.println("seleccione una opcion: ");
+        
+        // saca en pantalla una ventana emergente para elegir la opcion
         opc=Integer.parseInt(JOptionPane.showInputDialog("ingrese opcion: "));
-
+        
+        //ciclo if para ingresar los numeros en una ventana emergente
         if(opc>0&&opc<5){
-            dato1=Integer.parseInt(JOptionPane.showInputDialog("ingrese primer numero: "));
-            dato2=Integer.parseInt(JOptionPane.showInputDialog("ingrese segundo numero numero: "));
+            dato1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el primer numero: "));
+            dato2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el segundo numero numero: "));
+            
+        // otro ciclo if para que ejecute la operacion seleccionada
         if(opc!=0){
             if(opc==1)
                 result=dato1+dato2;
@@ -36,13 +42,17 @@ public class OperacionesBasicas {
                 result=dato1*dato2;
             if(opc==4)
                 result=dato1/dato2;
+            
+         //imprime en pantalla el resultado 
             System.out.print("el resultado es: ");
             System.out.println(result);
         }
+        
+        //imprime el resultado en la ventana emergente
         JOptionPane.showMessageDialog(null, "el resultado es: "+result);
         }
-            System.out.println("saliendo...");
-            JOptionPane.showMessageDialog(null,"adios");        
+            System.out.println("Cerrando el programa...");
+            JOptionPane.showMessageDialog(null,"Hasta luego");        
     }
     
 }
