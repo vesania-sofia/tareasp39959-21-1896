ecc476324d118d12db8b398f657532067906a78c
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-02-04 18:08:41 -0600

    revision y comentar el programa

--- PromedioNotas/PromedioNotas/src/main/java/PromedioNotas/Promedio.java
+++ PromedioNotas/PromedioNotas/src/main/java/PromedioNotas/Promedio.java
@@ -17,27 +17,35 @@ public class Promedio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner teclado= new Scanner(System.in);
-
+    System.out.print("PROGRAMA PARA PROMEDIO...");    
+// TODO code application logic here
+            Scanner teclado= new Scanner(System.in);
+        //Declaracion del Vector Prome
         int [] prome;
         prome= new int[20];
-        
-        int numdatos=20;
-       
+        //Declaración subindice de recorrido del vector Prome (contador)
         int i;
+        //Declaración de la variable que contendrá el promedio de notas (acumulador)
         float total=0;
+        //Recorrido del vector para la asignación de calificaciones o notas
         for(i=0;i<20;++i)
         {
-            System.out.print("ingrese calificacion del alumno "+i+": ");
+            System.out.print("ingrese calificacion del alumno en la posición ["+i+"]: ");
             prome[i]=teclado.nextInt();
-           total=total+prome[i];
+            total=total+prome[i];
         }
-        System.out.println("total es: "+total);
-        total= total/numdatos;
+        //Despliegue del promedio de notas o calificaciones
+        System.out.println("La suma de las notas es: "+total);
+        total= total/20;
         System.out.println("promedio general es: "+total);
+        //Despliegue de las notas desde el ultimo alumno ingresado hasta el primero
         for(i=19;i>=0;--i)
             System.out.println("alumno "+i+" su promedio es: "+prome[i]);  
-    }        
-        
-}
+        }
+    
+    
+    }
+    
+    
+         
+
